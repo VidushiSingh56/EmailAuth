697e8698751ede0943fca5391a0a0c396bf855d6
Vidushi <vidushi68ar@gmail.com>
2024-12-07 16:58:12 +0530

    Completed Login functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:projectemailauthdec1/wrapper.dart';
 
 void main()async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -34,7 +35,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Wrapper(),
     );
   }
 }
